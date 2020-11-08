f4a1f9bcf96f660a7f344e6c0ab7c8fcdfc4c510
dv258 <60153503+dv258@users.noreply.github.com>
2020-11-08 15:34:22 -0500

    add youtube event handlers

--- scripts/YoutubeContainer.jsx
+++ scripts/YoutubeContainer.jsx
@@ -6,15 +6,64 @@ import YouTube from 'react-youtube';
 
 
 export function YoutubeContainer() {
+	const [ytPlayer, setYtPlayer] = React.useState(null);
+	const ytPlayerRef = React.useRef();
+
+	ytPlayerRef.current = ytPlayer;
+
 	React.useEffect(() => {
 		Socket.on('yt-state-change', (data) => {
-			console.log(data);
+			switch(data.state)
+			{
+				case 'play':
+					ytPlayerRef.current.playVideo();
+					break;
+				case 'pause':
+					ytPlayerRef.current.pauseVideo();
+					break;
+			}
 		});
 	}, []);
 
+	function onYtReady(event)
+	{
+		setYtPlayer(event.target);
+	}
+
+	function onYtPlay(event)
+	{
+		console.log('play', event);
+
+		Socket.emit('yt-state-change', {
+			'state': 'play',
+			'offset': undefined
+		});
+	}
+
+	function onYtPause(event)
+	{
+		console.log('pause', event);
+
+		Socket.emit('yt-state-change', {
+			'state': 'pause',
+			'offset': undefined
+		});
+	}
+
+	function onYtStateChange(event)
+	{
+		console.log('state change', event);
+	}
+
 	return (
 		<div>
-			<YouTube />
+			<YouTube
+				videoId='dQw4w9WgXcQ'
+				onReady={onYtReady}
+				onPlay={onYtPlay}
+				onPause={onYtPause}
+				onStateChange={onYtStateChange}
+			/>
 		</div>
 	);
 }
