2cd0a347196b287a4fca1c04dd5ce9624568c41c
Matt Gonzalez <mgonz@pop-os.localdomain>
2020-11-09 21:56:48 -0500

    pushed chat skeleton

---
+++ message.py
@@ -0,0 +1,11 @@
+class Message:
+	def __init__(self, messageId, messageText, messageTimestamp, messageRoomId, messageUserId):
+		self.id = messageId
+		self.text = messageText
+		self.timestamp = messageTimestamp
+		self.roomId = messageRoomId
+		self.userId = messageUserId
+
+	def generateMessageId(self, length)
+		
+
