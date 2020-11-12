3cb8152e6a443b3a2fb8031236a5f308d830a962
gpeppel <ec2-user@ip-172-31-1-194.ec2.internal>
2020-11-12 19:25:37 +0000

    Add database model to user.py

--- user.py
+++ user.py
@@ -1,3 +1,18 @@
+import flask_sqlalchemy
+from app import db
+
 class User:
 	def __init__(self, userId):
 		self.id = userId
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120))
+    email = db.Column(db.String(120))
+    settings = db.Column(db.String(120))
+    
+    def __init__(self, i, n, e, s):
+        self.address = i
+        self.address = n
+        self.address = e
+        self.address = s
