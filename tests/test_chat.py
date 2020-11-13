531819f29946ec6fa92404aab5445cdcd5ad8790
dv258 <60153503+dv258@users.noreply.github.com>
2020-11-13 10:36:09 -0500

    fix app and chat tests

--- tests/test_chat.py
+++ tests/test_chat.py
@@ -1,53 +1,52 @@
+from datetime import datetime
 import unittest
 import unittest.mock as mock
 import sys
-sys.path.append("..")
-import app
 import time
-from datetime import datetime
+
+import app
 
 INPUT_MESSAGE = 'message'
 MESSAGE_EXPECTED = 'Message()_expected'
 
 class ChatTest(unittest.TestCase):
-    def setUp(self):
-        self.success_tests = [
-            {
-                INPUT_MESSAGE: {
-                    'text': 'test_message_text'
-                },
-                MESSAGE_EXPECTED: {
-                    'id': 1,
-                    'text': 'test_message_text',
-                    'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-                    'roomId': 'room_id_here',
-                    'userId': 1,
-                }
-            }
-        ]
+	@classmethod
+	def setUpClass(cls):
+		cls.flaskserver = app.createFlaskServer(app.db)
 
-    def mocked_db_add(self, message):
-        attribute_dict = {}
-        message_dict = message.__dict__
-        for key in message_dict.keys():
-            if not key.startswith('_'):
-                attribute_dict[key] = message_dict[key]
-        return attribute_dict
+	def setUp(self):
+		self.success_tests = [
+			{
+				INPUT_MESSAGE: {
+					'text': 'test_message_text'
+				},
+				MESSAGE_EXPECTED: {
+					'id': 1,
+					'text': 'test_message_text',
+					'timestamp': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+					'roomId': 'room_id_here',
+					'userId': 1,
+				}
+			}
+		]
 
-    @mock.patch('random.randint')
-    def test_parse_chat_message_success(self, mocked_id_generator):
-        for test in self.success_tests:
-                with mock.patch('app.add_to_db', self.mocked_db_add):
-                    mocked_id_generator.return_value = 1
-                    response = app.new_message_received(test[INPUT_MESSAGE])
-                    expected = test[MESSAGE_EXPECTED]
+	def mocked_db_add(self, message):
+		attribute_dict = {}
+		message_dict = message.__dict__
+		for key in message_dict.keys():
+			if not key.startswith('_'):
+				attribute_dict[key] = message_dict[key]
+		return attribute_dict
 
-                    self.assertEqual(response, expected)
+	@mock.patch('random.randint')
+	def test_parse_chat_message_success(self, mocked_id_generator):
+		for test in self.success_tests:
+				with mock.patch('socketns.youtube.YoutubeNamespace.add_to_db', self.mocked_db_add):
+					mocked_id_generator.return_value = 1
+					response = self.flaskserver.youtubeNs.on_message_send(test[INPUT_MESSAGE])
+					expected = test[MESSAGE_EXPECTED]
+
+					self.assertEqual(response, expected)
 
 if __name__ == "__main__":
-    unittest.main()
-
-
-
-
-
+	unittest.main()
--- tests/test_youtube.py
+++ tests/test_youtube.py
@@ -2,7 +2,11 @@ import random
 import unittest
 
 import app
+import tests.helpers as helpers
+from tests.helpers import MockRequest, hookSocketEmit
 
+
+TEST_SID = '69cbaae81f874b36ae9e24be92f79006'
 
 URL = 'url'
 ID = 'id'
@@ -34,9 +38,27 @@ YOUTUBE_VIDEO_IDS = [
 class YoutubeTest(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
-		cls.flaskserver = app.createFlaskServer(app.app, app.db)
+		cls.flaskserver = app.createFlaskServer(app.db)
 
 
 	def test_get_youtube_video_id(self):
 		for vobj in YOUTUBE_VIDEO_IDS:
 			self.assertEqual(self.flaskserver.youtubeNs.getYoutubeVideoId(vobj[URL]), vobj[ID])
+
+
+	def test_handle_yt_state_change(self):
+		testData = {
+			'state': 'unstarted'
+		}
+
+		mockReq = MockRequest(TEST_SID)
+
+		with hookSocketEmit() as emitList:
+			self.flaskserver.youtubeNs.connectUser(mockReq)
+			self.flaskserver.youtubeNs.handleYtStateChange(mockReq, testData)
+
+			print(emitList)
+
+			# TODO
+
+			self.flaskserver.youtubeNs.disconnectUser(mockReq)
