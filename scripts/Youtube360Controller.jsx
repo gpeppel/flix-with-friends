8a3498dc1ad0b728361f240a5913ffbc43d6a2b9
dv258 <60153503+dv258@users.noreply.github.com>
2020-11-30 19:18:34 -0500

    add keylistener.js

--- scripts/Youtube360Controller.jsx
+++ scripts/Youtube360Controller.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 
-import FrameUpdate from './utils/frame-update.js';
+import KeyListener from './utils/keylistener.js';
 
 
 const SPEED = 4;
@@ -10,41 +10,45 @@ export function Youtube360Controller(props)
 {
 	const player = props.player;
 
-	const keysDown = {};
+	function wrap(val, minVal, maxVal)
+	{
+		if(val >= minVal && val <= maxVal)
+			return val;
+		if(val < minVal)
+			return maxVal - (minVal - val);
+		return minVal + (val - maxVal);
+	}
 
 	//useEffect?
 
-	const keylistener = new FrameUpdate(() =>
+	const keylistener = new KeyListener(undefined, undefined, () =>
 	{
+		if(!player || !player.player)
+			return;
+
 		const sphereProp = player.player.getSphericalProperties();
 
 		if(sphereProp === undefined || Object.keys(sphereProp).length == 0)
 			return;
 
-		if(keysDown['a'])
-			sphereProp.yaw += SPEED;
-		if(keysDown['d'])
-			sphereProp.yaw -= SPEED;
-		if(keysDown['w'])
-			sphereProp.pitch += SPEED;
-		if(keysDown['s'])
-			sphereProp.pitch -= SPEED;
-		if(keysDown['q'])
-			sphereProp.roll += SPEED;
-		if(keysDown['e'])
-			sphereProp.roll -= SPEED;
+		if(keylistener.isKeyDown('a'))
+			sphereProp.yaw = wrap(sphereProp.yaw + SPEED, 0, 360);
+		if(keylistener.isKeyDown('d'))
+			sphereProp.yaw = wrap(sphereProp.yaw - SPEED, 0, 360);
+		if(keylistener.isKeyDown('w'))
+			sphereProp.pitch = Math.min(90, sphereProp.pitch + SPEED);
+		if(keylistener.isKeyDown('s'))
+			sphereProp.pitch = Math.max(-90, sphereProp.pitch - SPEED);
+		if(keylistener.isKeyDown('q'))
+			sphereProp.roll = wrap(sphereProp.roll + SPEED, -180, 180);
+		if(keylistener.isKeyDown('e'))
+			sphereProp.roll = wrap(sphereProp.roll - SPEED, -180, 180);
+		if(keylistener.isKeyDown('z'))
+			sphereProp.fov = Math.max(30, sphereProp.fov - SPEED);
+		if(keylistener.isKeyDown('x'))
+			sphereProp.fov = Math.min(120, sphereProp.fov + SPEED);
 
 		player.player.setSphericalProperties(sphereProp);
-	}, FrameUpdate.fps(60));
-
-	window.addEventListener('keydown', (event) =>
-	{
-		keysDown[event.key] = performance.now();
-	});
-
-	window.addEventListener('keyup', (event) =>
-	{
-		keysDown[event.key] = 0;
 	});
 
 	function onChange(event)
@@ -61,8 +65,8 @@ export function Youtube360Controller(props)
 
 	return (
 		<div>
-			<input type='checkbox' onChange={onChange} />
-			<label>360 Keyboard Control</label>
+			<input id='control360' type='checkbox' onChange={onChange} />
+			<label htmlFor='control360'>360 Keyboard Control</label>
 		</div>
 	);
 }
