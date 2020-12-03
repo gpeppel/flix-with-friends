d0432704179fc5d8477626c04a6a4d4e05327b5f
Matt Gonzalez <mgonz@pop-os.localdomain>
2020-12-03 14:17:18 -0500

    added enqueue frontend

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,56 +1,42 @@
 import * as React from 'react';
 import { Chat } from './Chat';
 import { YoutubeContainer } from './YoutubeContainer';
+import { Queue } from './Queue';
 
 import { Socket } from './Socket';
 
 import './css/content.css';
 
+export function Content() {
+  const [roomID, setRoomID] = React.useState([]);
 
-const EVENT_YT_LOAD = 'yt_load';
+  console.log('emitting');
+  Socket.emit('new room');
 
+  Socket.emit('chat_loaded');
 
-export function Content()
-{
-	const [roomID, setRoomID] = React.useState([]);
+  React.useEffect(() => {
+    Socket.on('new room id', setRoomID);
+  }, []);
+  console.log(roomID);
 
-	console.log('emitting');
-	Socket.emit('new room');
-
-	Socket.emit('chat_loaded');
-
-	React.useEffect(() =>
-	{
-		Socket.on('new room id', setRoomID);
-	}, []);
-	console.log(roomID);
-
-	function onKeyUp(event)
-	{
-		if(event.key == 'Enter')
-		{
-			Socket.emit(EVENT_YT_LOAD, {
-				'url': event.target.value
-			});
-		}
-	}
-
-	return (
-		<div className='main-content'>
-			<div style={{
-				display: 'flex',
-				flex: 1
-			}}>
-				<Chat />
-			</div>
-			<div className='media-area'>
-				<input onKeyUp={onKeyUp} placeholder="Enter YouTube URL"/>
-				<YoutubeContainer />
-			</div>
-			<div style={{
-				flex: 1
-			}}>
-			</div>
-		</div>
-	);
+  return (
+    <div className="main-content">
+      <div style={{
+        display: 'flex',
+        flex: 1,
+      }}
+      >
+        <Chat />
+      </div>
+      <div className="media-area">
+        <YoutubeContainer />
+        <Queue />
+      </div>
+      <div style={{
+        flex: 1,
+      }}
+      />
+    </div>
+  );
 }
