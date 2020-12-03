028e85030694b647f745041f519f26006e7fb247
dv258 <60153503+dv258@users.noreply.github.com>
2020-12-03 13:05:30 -0500

    sent room settings on save

--- scripts/HostOptions.jsx
+++ scripts/HostOptions.jsx
@@ -37,22 +37,42 @@ export function HostOptions()
 
 	function onSaveChanges()
 	{
-		Socket.emit('room_settings_set', {
+		Socket.emit('room_settings_set', getRoomSettings());
+	}
 
-		});
+	function getRoomSettings()
+	{
+		const voteThresholdPercentage = document.getElementById('vote-threshold-percentage');
+		const voteThreshold = document.getElementById('vote-threshold');
+
+		const settings = {};
+
+		try
+		{
+			settings.voteThreshold = parseFloat(
+				voteThresholdPercentage.checked ? voteThreshold.value / 100 : voteThreshold.value,
+			);
+		}
+		catch(e)
+		{
+			/* eslint-disable no-empty */
+		}
+
+		console.log(settings);
+		return settings;
 	}
 
 	return (
 		<div id='host-options'>
 			<div>
 				<div>
-					<input id='vote-threshold-percentage' onChange={onUsePercentage} type='checkbox' />
-					<label htmlFor='vote-threshold-percentage'>Use Percentage</label>
-				</div>
-				<div>
 					<label htmlFor='vote-threshold'>Vote Threshold: </label>
 					<input id='vote-threshold' type='number' min='0'/>
 					<span id='vote-percent-sign'>%</span>
+				</div>
+				<div>
+					<input id='vote-threshold-percentage' onChange={onUsePercentage} type='checkbox' />
+					<label id='lbl-vote-threshold-percentage' htmlFor='vote-threshold-percentage'>Use Percentage</label>
 				</div>
 			</div>
 
--- socketns/room.py
+++ socketns/room.py
@@ -2,6 +2,9 @@ import flask
 import flask_socketio
 
 
+ROOM_SETTINGS_GET = 'room_settings_get'
+
+
 class RoomNamespace(flask_socketio.Namespace):
     def __init__(self, namespace, server):
         super().__init__(namespace)
@@ -49,3 +52,13 @@ class RoomNamespace(flask_socketio.Namespace):
             'description': room.description,
             'current_video_code': room.get_current_video_code()
         }
+
+    def on_room_settings_set(self, data):
+        user = self.flaskserver.get_user_by_request(flask.request)
+
+        try:
+            user.room.set_vote_threshold(data['voteThreshold'])
+        except:
+            pass
+
+        user.room.emit(ROOM_SETTINGS_GET, {})
