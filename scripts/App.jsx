ee143bd07d9707016e667937ba0a9b9bd401192c
gpeppel <ec2-user@ip-172-31-1-194.ec2.internal>
2020-12-04 01:06:58 +0000

    Merge login --> Develop

--- scripts/App.jsx
+++ scripts/App.jsx
@@ -1,25 +1,17 @@
 import * as React from 'react';
 
 import { Login } from './Login';
+import { Header } from './Header';
+import { Footer } from './Footer';
 
 
 export function App()
 {
 	return (
-		<div style={{
-			height: '100%'
-		}}>
-			<div style={{
-				display: 'flex',
-				flexDirection: 'column',
-				height: '100%'
-			}}>
-				<div className='main-header'>
-					<img className='logo' src='/static/images/logo.png' alt='logo' />
-				</div>
-
-				<Login />
-			</div>
+		<div>
+			<Header />
+			<Login  />
+			<Footer />
 		</div>
 	);
 }
