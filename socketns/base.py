37f64cdf279c34e538e9944841052171321048c3
gpeppel <peppel.greg@gmail.com>
2020-12-08 19:31:59 -0500

    l

--- socketns/base.py
+++ socketns/base.py
@@ -37,7 +37,7 @@ class BaseNamespace(flask_socketio.Namespace):
             return
             
         print('----------------------')
-        print(str(user.username) + ' disconnected from room' )
+        print(str(user.username) + '  disconnected from room' )
         print('----------------------')
 
         room = user.room
